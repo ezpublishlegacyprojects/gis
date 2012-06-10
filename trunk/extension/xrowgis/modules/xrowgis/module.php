e9da8de797deb209f7dd83f2966b47cc020d2f95
Björn Dieding <bjoern@xrow.de>
2012-06-10 18:33:51 +0000

    git-svn-id: http://svn.projects.ez.no/gis@68 c0da6fad-dbba-4ecd-8c1f-74d516873d0d

--- trunk/extension/xrowgis/modules/xrowgis/module.php
+++ trunk/extension/xrowgis/modules/xrowgis/module.php
@@ -4,8 +4,24 @@ $Module = array(
 );
 $ViewList["georss"] = array( 
     "script" => "georss.php" , 
-    'params' => array(
+    'params' => array( 
         'NodeID' 
+    ) 
+);
+
+$ViewList['relations'] = array( 
+    'functions' => array( 
+        'editor' 
+    ) , 
+    'ui_context' => 'edit' , 
+    'script' => 'relations.php' , 
+    'params' => array( 
+        'ObjectID' , 
+        'ObjectVersion' , 
+        'ContentType' , 
+        'EmbedID' , 
+        'EmbedInline' , 
+        'EmbedSize' 
     ) 
 );
 
@@ -19,12 +35,13 @@ $ViewList['upload'] = array(
         'ObjectID' , 
         'ObjectVersion' , 
         'ContentType' , 
-        'AttributeID' , 
+        'AttributeID' 
     ) 
 );
 
 $FunctionList = array();
 $FunctionList['georss'] = array();
+$FunctionList['relations'] = array();
 $FunctionList['editor'] = array();
 $FunctionList['search'] = array(); // only used by template code to see if user should see this feature in ezoe
 $FunctionList['browse'] = array(); // only used by template code to see if user should see this feature in ezoe
